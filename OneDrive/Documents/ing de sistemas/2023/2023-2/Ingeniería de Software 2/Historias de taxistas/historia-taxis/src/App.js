9848ad181950d792e2f152b041cefca1988a05a0
adrianaMurillo19 <adrianal.murillot@uqvirtual.edu.co>
2023-09-17 21:36:30 -0500

    se instalo nuevos complementos react-router-dom@5.2.0 para navegar de inicio a varias paginas

--- OneDrive/Documents/ing de sistemas/2023/2023-2/Ingeniería de Software 2/Historias de taxistas/historia-taxis/src/App.js
+++ OneDrive/Documents/ing de sistemas/2023/2023-2/Ingeniería de Software 2/Historias de taxistas/historia-taxis/src/App.js
@@ -1,34 +1,85 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 
-import { RegistroForm } from './RegistroForm';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
+
+
+
+/* import { RegistroForm} from './Componentes/RegistroForm'; */
 
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
 
-          {/* Ensayo */}
+     <React.Fragment>
 
-          <RegistroForm></RegistroForm>
+    
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              {/* <Link to="/">Inicio</Link> */}
+            </li>
+            <li>
+              {/* <Link to="/registro">Registro</Link> */}
+            </li>
+            <li>
+              {/* <Link to="/iniciar-sesion">Iniciar sesión</Link> */}
+            </li>
+          </ul>
+        </nav>
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switch>
+          <Route path="/registro">
+            <Registro />
+          </Route>
+          <Route path="/iniciar-sesion">
+            <IniciarSesion />
+          </Route>
+          <Route path="/">
+            <Inicio />
+          </Route>
+        </Switch>
+      </div>
+    
+    </Router>
+    </React.Fragment>
+  )  ;
+}
+
+function Inicio() {
+  return (
+    <div>
+      <h2>Inicio</h2>
+      <p>Bienvenido a la página de inicio.</p>
     </div>
   );
 }
 
+function Registro() {
+  return (
+    <div>
+      <h2>Registro</h2>
+      {/* Aquí iría el formulario de registro */}
+    </div>
+  );
+}
+
+function IniciarSesion() {
+  return (
+    <div>
+      <h2>Iniciar sesión</h2>
+      {/* Aquí iría el formulario de inicio de sesión */}
+    </div>
+  );
+}
+
+
 export default App;
+
